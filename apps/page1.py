56f0c412397b5d43fb3b0fedac647f592473d73d
Ludo-R <ludovic.randon@icloud.com>
2020-12-15 09:27:48 +0100

    add visu

--- apps/page1.py
+++ apps/page1.py
@@ -14,7 +14,9 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
-
+import plotly.express as px
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
 from app import app
 
 # ---------------------------------------------------
@@ -28,7 +30,7 @@ df2 = pd.read_csv("data/text_emotion.csv")
 trace = go.Histogram(x=df["Emotion"], xbins=dict(),
                    marker=dict(color='#810303'))
 layout = go.Layout(
-    title="Frequency Emotions Counts From First Table"
+    title="Frequency Emotions Counts From Kaggle Data"
 )
 fig = go.Figure(data=go.Data([trace]), layout=layout)
 
@@ -36,9 +38,63 @@ fig = go.Figure(data=go.Data([trace]), layout=layout)
 trace = go.Histogram(x=df2["sentiment"], xbins=dict(),
                    marker=dict(color='#11337E'))
 layout = go.Layout(
-    title="Frequency Emotions Counts From Second Table"
+    title="Frequency Emotions Counts From Word Data"
 )
 fig2 = go.Figure(data=go.Data([trace]), layout=layout)
+
+# - Figure 3 -
+
+x = np.array(df["Text"])
+y = np.array(df["Emotion"])
+vec = CountVectorizer(ngram_range=(1,2), min_df=500 )
+X = vec.fit_transform(x)
+words = vec.get_feature_names()
+wsum = np.array(X.sum(0))[0]
+ix = wsum.argsort()[::-1]
+wrank = wsum[ix] 
+labels = [words[i] for i in ix]
+def subsample(x, step=150):
+    return np.hstack((x[:30], x[10::step]))
+freq = subsample(wrank)
+r = np.arange(len(freq))
+a = [i for i in range(100)]
+fig3 = px.bar(x=r, y=freq)
+fig3.update_layout(
+    xaxis = dict(
+        tickmode = 'array',
+        tickvals=a,
+        title_text="Words",
+        ticktext = subsample(labels)),
+    title="Frequency of word in Kaggel Data",
+    yaxis = dict(
+        title_text="Frequency",))
+
+# - Figure 4 -
+
+x = np.array(df2["content"])
+y = np.array(df2["sentiment"])
+vec = CountVectorizer(ngram_range=(1,2), min_df=500 )
+X = vec.fit_transform(x)
+words = vec.get_feature_names()
+wsum = np.array(X.sum(0))[0]
+ix = wsum.argsort()[::-1]
+wrank = wsum[ix] 
+labels = [words[i] for i in ix]
+def subsample(x, step=150):
+    return np.hstack((x[:30], x[10::step]))
+freq = subsample(wrank)
+r = np.arange(len(freq))
+a = [i for i in range(100)]
+fig4 = px.bar(x=r, y=freq)
+fig4.update_layout(
+    xaxis = dict(
+        tickmode = 'array',
+        tickvals=a,
+        title_text="Words",
+        ticktext = subsample(labels)),
+    title="Frequency of word in World Data",
+    yaxis = dict(
+    title_text="Frequency",))
 
 # - Tabs -
 tab1_content = dash_table.DataTable(id='container-button-timestamp',
@@ -92,11 +148,24 @@ layout = html.Div([
             dbc.Col(html.H1(children='Visualisation'), className="mb-2")
         ]),
         dbc.Row([
-            dbc.Col(html.H6(children='In first lets see the count of emotion'), className="mb-4")
+            dbc.Col(html.H4(children='In first lets see the count of emotion'), className="mb-1")
         ]), 
         dbc.Row([
                 dbc.Col(dcc.Graph(id='graph-1',figure=fig),),
-                dbc.Col(dcc.Graph(id='graph-2',figure=fig2),)
+                dbc.Col(dcc.Graph(id='graph-2',figure=fig2), className="mb-2"),
+            ]),
+        dbc.Row([
+                dbc.Col(html.H6(children='We can observe that the Happy and Sadness emotions are very present in the first dataset, in the second they are rather in the middle, it is neutral and worry which are dominant. Two emotions that do not exist in the first data set. The two datasets are distributed very differently'), className="mb-4")
+            ]),
+        dbc.Row([
+                dbc.Col(html.H4(children='Now watching the word frequency of the two dataset'), className="mb-1")
         ]), 
-        ])
+        dbc.Row([
+                dbc.Col(dcc.Graph(id='graph-3',figure=fig3),),
+                dbc.Col(dcc.Graph(id='graph-4',figure=fig4), className="mb-2"),
+            ]),
+        dbc.Row([
+                dbc.Col(html.H6(children='By taking the words that appear more than 500 times in the text, we realize that most of the words are linked words which cannot express any emotions, and that in the two sets of data, we must remove them with stopword if we want to make our model relevant.'), className="mb-4")
+            ]),
+        ]), 
     ])
--- apps/page2.py
+++ apps/page2.py
@@ -6,3 +6,31 @@ Created on Mon Dec 14 12:04:54 2020
 @author: randon
 """
 
+import plotly.graph_objects as go
+import pandas as pd
+import dash_table
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import SGDClassifier
+import nltk
+
+#-----------------------------------------
+# - Layout -
+
+layout = html.Div([
+    dbc.Container([
+        dbc.Row([
+            dbc.Col(html.H1(children='Machine learning'), className="mb-2")
+        ]),
+        dbc.Row([
+            dbc.Col(html.H6(children='We will compare and analyse different model'), className="mb-4")
+        ]),
+        ])
+    ])
